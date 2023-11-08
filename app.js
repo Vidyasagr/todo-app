d12e4d48244a654546d8c6bc5e8a71818b879bd4
Vidyasagr <101058908+Vidyasagr@users.noreply.github.com>
2023-11-08 16:52:14 +0530

    layouts

--- app.js
+++ app.js
@@ -6,20 +6,15 @@ app.set("view engine", "ejs");
 app.use(express.static('public'));
 app.use(express.urlencoded({extended:true}))
 
-var items =[];
-var example ="working";
+
 app.get("/", function(req,res){
-    // res.send("This web page is working file !")
-    res.render("list",{ejes:items});
+    
+    res.render("home");
 });
 
-app.post("/", function(req,res){
-    // console.log(req.body.ele1);
-    var item = req.body.ele1;
-    items.push(item);
-    res.redirect("/")
-
-})
+app.get("/about", function(req,res){
+    res.render("about");
+});
 
 app.listen(8000, function(){
     console.log("Server Started");
